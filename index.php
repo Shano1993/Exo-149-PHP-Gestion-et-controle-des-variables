f5ca697feb28c8a36b49eea663cdf5adda0e8367
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-13 09:46:38 +0100

    php

--- index.php
+++ index.php
@@ -7,9 +7,12 @@
  */
 $var = 0;
 // TODO votre code ici.
-
-
-
+if(empty($var)) {
+    echo "$var vaut 0 donc n'est pas vide";
+}
+else {
+    echo "$var est vide";
+}
 
 /**
  * 2. Détruisez la variable déclarée, tentez de l'afficher ensuite en utilisant un print_r.
@@ -17,21 +20,23 @@ $var = 0;
 $eraseMe = "Please erase me !";
 // TODO votre code ici.
 
+unset($eraseMe);
+print_r($eraseMe);
 
 /**
  * 3. Déclarez vous même un tableau et utilisez var_dump pour afficher toutes les informations de debug.
  */
 // TODO votre code ici.
-
+$array = ["Un", "Deux", "Trois", "Quatre"];
+var_dump($array);;
 echo "<br>";
 
 /**
  * 4. Faites la même chose avec le même tableau, mais pour la méthode print_r.
  */
 // TODO votre code ici.
-
+print_r($array);
 echo "<br>";
-
 
 /**
  * 5. A l'aide de la méthode isset, testez si la clé du tableau associatif 'doNotExists' existe ( SANS TOUCHER AU TABLEAU )
@@ -39,6 +44,14 @@ echo "<br>";
  */
 $tab = ["test" => true, "name" => "Doe", "age" => 32];
 // TODO Votre code ici.
+$key = "doNotExists";
+if (array_key_exists($key, $tab)) {
+    echo "La clé doNoExists existe" . "<br>";
+}
+else {
+    echo "La clé doNoExists n'existe pas" . "<br>";
+};
+
 
 
 /**
@@ -63,3 +76,28 @@ $tab = ["test" => true, "name" => "Doe", "age" => 32];
  */
 
 // TODO votre code ici.
+
+$bool = true;
+$nmbr1 = 45;
+$nmbr2 = 22.5;
+$str = "Je suis une chaîne de caractère";
+
+function verification($param) {
+    if (is_string($param)) {
+        echo "La variable $param est de type string" . "<br>";
+    }
+    elseif (is_integer($param)) {
+        echo "La variable $param est de type entier" . "<br>";
+    }
+    elseif (is_float($param)) {
+        echo "La variable $param est de type flottant" . "<br>";
+    }
+    elseif (is_bool($param)) {
+        echo "La variable $param est de type booléen" . "<br>";
+    }
+}
+
+verification($str);
+verification($nmbr1);
+verification($nmbr2);
+verification($bool);
